a83202556a85e0d1ab666f57ca6e6dd7018e2c58
joserodrigoSP <jrodris2005@gmail.com>
2024-04-28 22:58:07 -0600

    segunda version de primera parte
    a

--- Program.cs
+++ Program.cs
@@ -4,19 +4,25 @@
     {
 
      bool[,] ubicacionpieza = new bool[8,8]; //matriz para almacenar la posicion de las piezas del tablero.
-        int tipoPieza =0; int opcion=0; string pieza =""; 
+        int tipoPieza =0; int opcion=0; int numpieza =0;int k =0; int l=1;
+        
+        
+        Console.WriteLine("Cuantas piezas desea agregar?");
+        int.TryParse(Console.ReadLine(), out numpieza);
+        string[] pieza = new string[numpieza]; 
         do{
         Console.WriteLine("Qué tipo de pieza desea agregar? \n 1) alfil blanco \n 2) peón blanco \n 3) caballo blanco\n 4) torre blanca \n 5) alfil negro \n 6) peón negro \n 7) caballo negro\n 8) torre negra");
         int.TryParse(Console.ReadLine(), out tipoPieza);
         switch(tipoPieza){ 
-            case 1: pieza = "alfil blanco";break;
-            case 2: pieza = "peon blanco";break;
-            case 3: pieza = "caballo blanco";break;
-            case 4: pieza = "torre blanca";break;
-            case 5: pieza = "alfil negro";break;
-            case 6: pieza = "peon negro";break;
-            case 7: pieza = "caballo negro";break;
-            case 8: pieza = "torre negra";break;} // ASIGNAR LA PIEZA
+            
+            case 1: pieza[k] = "alfil blanco";break;
+            case 2: pieza[k] = "peon blanco";break;
+            case 3: pieza[k] = "caballo blanco";break;
+            case 4: pieza[k] = "torre blanca";break;
+            case 5: pieza[k] = "alfil negro";break;
+            case 6: pieza[k] = "peon negro";break;
+            case 7: pieza[k] = "caballo negro";break;
+            case 8: pieza[k] = "torre negra";break;} // ASIGNAR LA PIEZA
 
     //ubicacion de la pieza
         Console.WriteLine("En qué ubicación desea colocar la pieza?");
@@ -30,8 +36,8 @@
             } 
         }
         }
-       Console.WriteLine("Desea agregar otra pieza? \n 1) si \n 2) no"); //agregar otra pieza
-      int.TryParse(Console.ReadLine(), out opcion);}while (opcion!=2);
+        Console.WriteLine("El numero de pieza es: "+ numpieza+ ". Y el numero l es: "+ l);
+        k++;l++;}while (l <= numpieza);
 
         
 
@@ -39,6 +45,11 @@
             for(int j=0; j<ubicacionpieza.GetLength(1); j++){
                 Console.WriteLine("-"+ ubicacionpieza[j,i]);
             } 
+        }
+
+
+        for(int i=0; i<pieza.Length; i++){
+           Console.WriteLine(pieza[i]);
         }
       
 
